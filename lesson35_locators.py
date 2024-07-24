2526245d2d983376d393afd5b723bae90b85e075
vinimj18 <viniciusmaggiotto@gmail.com>
2024-07-24 17:16:36 +0200

    Update lesson 35 with Xpath and CSS Selectors

--- lesson35_locators.py
+++ lesson35_locators.py
@@ -12,5 +12,14 @@ driver.find_element(By.NAME, 'email').send_keys('vinimj_lixo18@gmail.com')
 driver.find_element(By.ID, 'exampleInputPassword1').send_keys('123456')
 driver.find_element(By.ID, 'exampleCheck1').click()
 
+# Creating an Xpath -> //tagname[@attribute='value']
+# Creating an CSSSelector -> tagname[attribute='value']
+driver.find_element(
+    By.CSS_SELECTOR, "input[name='name']").send_keys('Vinicius')
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+message = driver.find_element(By.CLASS_NAME, 'alert-success').text
+print(message)
+
+assert 'Success' in message
 
 time.sleep(5)
