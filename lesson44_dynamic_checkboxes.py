5484d9fc173895d2429a750268766820952b30a7
vinimj18 <viniciusmaggiotto@gmail.com>
2024-07-29 20:49:51 +0200

    Add select radio buttons with index and displayed

--- lesson44_dynamic_checkboxes.py
+++ lesson44_dynamic_checkboxes.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://rahulshettyacademy.com/AutomationPractice/')
 
+# Use this if you don't know the order, or if the options order can change.
+
 checkboxes = driver.find_elements(By.XPATH, '//input[@type="checkbox"]')
 
 for checkbox in checkboxes:
@@ -13,5 +15,20 @@ for checkbox in checkboxes:
         checkbox.click()
         assert checkbox.is_selected()
         break
+
+
+# Use this if you know that the options will always be in the same order.
+
+radiobuttons = driver.find_elements(By.CSS_SELECTOR, '.radioButton')
+radiobuttons[2].click()
+assert radiobuttons[2].is_selected()
+
+
+# Check if an element is displayed on the screen
+
+assert driver.find_element(By.ID, 'displayed-text').is_displayed()
+driver.find_element(By.ID, 'hide-textbox').click()
+assert not driver.find_element(By.ID, 'displayed-text').is_displayed()
+
 
 time.sleep(2)
