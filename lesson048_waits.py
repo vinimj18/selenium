8702b81d67d21bdddd10edc1b973c62d56ac622b
vinimj18 <viniciusmaggiotto@gmail.com>
2024-07-30 18:36:45 +0200

    Added explicit wait

--- lesson048_waits.py
+++ lesson048_waits.py
@@ -1,16 +1,20 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 driver = webdriver.Chrome()
 driver.get('https://rahulshettyacademy.com/seleniumPractise/#/')
 
 # The code will wait 5 seconds or less to execute the code (Max timeout)
 # If the element is identified on the page, it will execute before the 5 seconds
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 
 driver.find_element(By.CSS_SELECTOR, '.search-keyword').send_keys('ber')
+
+# keep the 'sleep' for find_elements, or the implicity wait can return an empty
+# or incomplete list.
 time.sleep(2)
 items = driver.find_elements(By.XPATH, '//div[@class="products"]/div')
 count_items = len(items)
@@ -25,4 +29,11 @@ driver.find_element(By.XPATH, '//button[text()="PROCEED TO CHECKOUT"]').click()
 driver.find_element(By.CSS_SELECTOR, '.promoCode').send_keys(
     'rahulshettyacademy')
 driver.find_element(By.CSS_SELECTOR, '.promoBtn').click()
+
+# Explicit wait:
+# This srep takes longer than the implicit wait. So we add an explicit wait just
+# for this specific step
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located(
+    (By.CSS_SELECTOR, '.promoInfo')))
 print(driver.find_element(By.CSS_SELECTOR, '.promoInfo').text)
