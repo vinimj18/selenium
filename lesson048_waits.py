94a4f29a290674fcfe48f2d1ad38cf057e0051ef
vinimj18 <viniciusmaggiotto@gmail.com>
2024-07-30 18:13:22 +0200

    added implicity_wait

--- lesson048_waits.py
+++ lesson048_waits.py
@@ -6,6 +6,10 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://rahulshettyacademy.com/seleniumPractise/#/')
 
+# The code will wait 5 seconds or less to execute the code (Max timeout)
+# If the element is identified on the page, it will execute before the 5 seconds
+driver.implicitly_wait(5)
+
 driver.find_element(By.CSS_SELECTOR, '.search-keyword').send_keys('ber')
 time.sleep(2)
 items = driver.find_elements(By.XPATH, '//div[@class="products"]/div')
@@ -18,9 +22,7 @@ for item in items:
 driver.find_element(By.CSS_SELECTOR, '.cart-icon').click()
 driver.find_element(By.XPATH, '//button[text()="PROCEED TO CHECKOUT"]').click()
 
-time.sleep(2)
 driver.find_element(By.CSS_SELECTOR, '.promoCode').send_keys(
     'rahulshettyacademy')
 driver.find_element(By.CSS_SELECTOR, '.promoBtn').click()
-time.sleep(8)
 print(driver.find_element(By.CSS_SELECTOR, '.promoInfo').text)
