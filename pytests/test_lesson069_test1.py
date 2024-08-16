2a4118fbc6cc90b0eb2665230ce790d035363620
vinimj18 <viniciusmaggiotto@gmail.com>
2024-08-16 17:33:28 +0200

    created pytest folder

---
+++ pytests/test_lesson069_test1.py
@@ -0,0 +1,9 @@
+# Pytest file names should start with 'test_'
+# Test methods should also start with 'test_'
+
+def test_say_hello():
+    print("Hello")
+
+
+def test_say_hi():
+    print("Hi")
---
+++ pytests/test_lesson069_test2.py
@@ -0,0 +1,2 @@
+def test_is_equal_to():
+    assert 1 + 1 == 2
