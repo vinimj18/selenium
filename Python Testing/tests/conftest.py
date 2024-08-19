c0b504f859da3e301421c29481816bf456c11167
vinimj18 <viniciusmaggiotto@gmail.com>
2024-08-19 18:10:57 +0200

    Added baseclass

--- Python Testing/tests/conftest.py
+++ Python Testing/tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
 
 
-@pytest.fixture(scope='class')
-def setup_browser():
+@pytest.fixture(scope="class")
+def setup(request):
     chrome_options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.maximize_window()
+    request.cls.driver = driver
+    yield
+    driver.close()
